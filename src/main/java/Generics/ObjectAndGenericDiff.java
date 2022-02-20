f24ea999bb0279cacb9c30ca98b137b66f5d6673
Sergey Rudov <srudov@accesssoftek.com>
2022-02-20 16:41:44 +0200

    - added examples

---
+++ src/main/java/Generics/ObjectAndGenericDiff.java
@@ -0,0 +1,35 @@
+package Generics;
+
+import java.util.List;
+
+public class ObjectAndGenericDiff {
+    public static void main(String[] args) {
+        Account a1 = new Account(5.97, 150, List.of(1));
+        Account a2 = new Account(44, 800, List.of(2));
+
+        System.out.println(a1.getId());
+        System.out.println(a2.getId());
+    }
+}
+
+
+
+
+
+
+class Account <T extends Number>{
+
+    T id;
+    private int sum;
+    private List<?> dt;
+
+    public Account(T id, int sum, List<?> dt) {
+        this.id = id;
+        this.sum = sum;
+        this.dt = dt;
+    }
+
+    public Object getId() { return id; }
+    public int getSum() { return sum; }
+    public void setSum(int sum) { this.sum = sum; }
+}
